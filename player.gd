ae4006ae6e665e585263a7180824766e529f541f
M-R-Jacobs <mjacobs0002@gmail.com>
2026-08-16 11:23:41 -0700

    add player facing rotation

--- player.gd
+++ player.gd
@@ -2,6 +2,9 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const ROTATION_SPEED = 10.0
+
+@onready var model = $PC
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -12,9 +15,14 @@ func _physics_process(delta: float) -> void:
 
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		
+		var target_angle = atan2(direction.x, direction.z)
+		model.rotation.y = lerp_angle(model.rotation.y, target_angle, ROTATION_SPEED * delta)
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
